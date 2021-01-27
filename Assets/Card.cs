c3af90dd49fc48c48eb009296d9dea0bb50b82a4
RichardDufour <richarddufour@laposte.net>
2021-01-27 14:32:38 +0100

    Initial commit

---
+++ Assets/Card.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class Card : MonoBehaviour
+{
+    public int Id;
+    public string CardName;
+    public int Cost;
+    public int Power;
+    public string CardDescription;
+
+    public Card()
+	{
+
+	}
+
+    public Card(int id, string cardName, int cost, int power, string cardDescription)
+	{
+        Id = id;
+        CardName = cardName;
+        Cost = cost;
+        Power = power;
+        CardDescription = cardDescription;
+	}
+}
